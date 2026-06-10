b9a8d03c78401b15582b31311fcd1751190ec1fc
Anirban Deb Singha <anirban12321@gmail.com>
2026-06-10 21:43:13 +0530

    feat: add Diffview integration

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -14,4 +14,54 @@ return {
       current_line_blame = false,
     },
   },
+  {
+    "sindrets/diffview.nvim",
+    cmd = {
+      "DiffviewClose",
+      "DiffviewFileHistory",
+      "DiffviewFocusFiles",
+      "DiffviewOpen",
+      "DiffviewRefresh",
+      "DiffviewToggleFiles",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    keys = {
+      {
+        "9",
+        function()
+          local view = require("diffview.lib").get_current_view()
+          vim.cmd(view and "DiffviewClose" or "DiffviewOpen")
+        end,
+        desc = "Git: Toggle Diff View",
+      },
+    },
+    opts = {
+      enhanced_diff_hl = true,
+      view = {
+        default = {
+          layout = "diff2_horizontal",
+          winbar_info = true,
+        },
+        merge_tool = {
+          layout = "diff3_mixed",
+          disable_diagnostics = true,
+          winbar_info = true,
+        },
+        file_history = {
+          layout = "diff2_horizontal",
+          winbar_info = true,
+        },
+      },
+      file_panel = {
+        listing_style = "tree",
+        win_config = {
+          position = "left",
+          width = 35,
+        },
+      },
+    },
+  },
 }
